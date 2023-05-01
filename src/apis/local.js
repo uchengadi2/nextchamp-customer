696f7b0fb4c2813793fd1e7baa39aaed3d762212
Uche Ngadi <uchengadi@yahoo.com>
2023-05-01 19:18:01 +0100

    repoint base url to live

--- src/apis/local.js
+++ src/apis/local.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:9005/api/v1", //for the localhost
-  //baseURL: "https://api.eshieldafrica.com/api/v1", // for production
+  //baseURL: "http://localhost:9005/api/v1", //for the localhost
+  baseURL: "https://api.nextchamp.co/api/v1", // for production
 });
